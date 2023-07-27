0708172455f1b6d9ad598a64dd1dbd8fc7196b52
Satyam Kumar <satyam.kumar@rackspace.com>
2023-07-27 19:25:55 +0530

    updated

--- basic_cosmos_task_group.py
+++ basic_cosmos_task_group.py
@@ -15,7 +15,7 @@ from cosmos.profiles import SnowflakeUserPasswordProfileMapping
 
 DEFAULT_DBT_ROOT_PATH = Path(__file__).parent / "dbt"
 DBT_ROOT_PATH = Path(os.getenv("DBT_ROOT_PATH", DEFAULT_DBT_ROOT_PATH))
-
+print(DBT_ROOT_PATH)
 profile_config = ProfileConfig(
     profile_name="default",
     target_name="dev",
