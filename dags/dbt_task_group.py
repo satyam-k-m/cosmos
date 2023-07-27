944e994eafd2c7938437d3c49acbca2890014aba
satyam-k-m <136057137+satyam-k-m@users.noreply.github.com>
2023-07-27 11:54:27 +0530

    Create dbt_task_group.py

---
+++ dags/dbt_task_group.py
@@ -0,0 +1,55 @@
+
+# from airflow.operators.empty import EmptyOperator
+# import os
+
+
+# """
+# ### Run a dbt Core project as a task group with Cosmos
+
+# Simple DAG showing how to run a dbt project as a task group, using
+# an Airflow connection and injecting a variable into the dbt project.
+# """
+
+# from airflow.decorators import dag
+# from cosmos.providers.dbt.task_group import DbtTaskGroup
+# from pendulum import datetime
+
+CONNECTION_ID = "snowlake_default"
+DB_NAME = "DFS_POC_DB"
+SCHEMA_NAME = "P_DATA"
+DBT_PROJECT_NAME = "developement"
+# the path where Cosmos will find the dbt executable
+
+DBT_EXECUTABLE_PATH = "/home/airflow/.local/bin/dbt"
+# The path to your dbt root directory
+# HOME = os.environ["HOME"] # retrieve the location of your home folder
+# DBT_ROOT_PATH = os.path.join(HOME,  "DFS/dbt_project/dbt/dbt/") # path to your dbt project
+DBT_ROOT_PATH = "dbt_path = "/opt/airflow/git/cosmos.git/dbt"
+
+
+from pendulum import datetime
+
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from cosmos.providers.dbt.task_group import DbtTaskGroup
+
+
+with DAG(
+    dag_id="dbt_task_group",
+    start_date=datetime(2022, 11, 27),
+    schedule="@daily",
+):
+    e1 = EmptyOperator(task_id="pre_dbt")
+
+    dbt_tg = DbtTaskGroup(
+        dbt_project_name="development",
+        conn_id="snowflake_default",
+        dbt_args={
+            "dbt_executable_path": DBT_EXECUTABLE_PATH,
+            "schema": "public",
+        },
+    )
+
+    e2 = EmptyOperator(task_id="post_dbt")
+
+    e1 >> dbt_tg >> e2
